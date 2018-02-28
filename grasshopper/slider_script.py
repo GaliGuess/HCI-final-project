b93497c42b68b0067a0a0e331c87ad4b5afd01d1
GiladSchreiber <35434081+GiladSchreiber@users.noreply.github.com>
2018-02-28 18:21:48 +0200

    almost done

--- grasshopper/slider_script.py
+++ grasshopper/slider_script.py
@@ -7,14 +7,14 @@ from time import sleep
 
 refresh_time = 0.1
 
-F0_GUID = "80f4774d-1225-4aec-84f3-55fd4990833d"
-F1_GUID = "b04a6c7b-b04d-488b-9ab7-9ab99d8f4405"
-F2_GUID = "57bad36d-e431-4fa2-8c56-bfc332b37074"
-F3_GUID = "4498d06a-f050-4ccb-a548-e579fb053c3f"
-F4_GUID = "59887ad7-a1ce-4f33-8142-22cbfddadc2e"
-F5_GUID = "6b60494d-baaa-4ba2-8f6d-ceac3fcb666b"
-F6_GUID = "811bd9cb-dd5a-44ba-a36c-28acfff21cb7"
-F7_GUID = "c681f240-b1bc-4032-b9c7-0a6b5017f1fc"
+F0_GUID = "f5b64d4e-911e-41ee-bee8-7f51323c3c5f"
+F1_GUID = "db88242f-867b-41d2-b0a8-90a532a104eb"
+F2_GUID = "925f05d1-1985-4a58-8f03-a2cde8a1851c"
+F3_GUID = "ceb9bd12-8f4a-40e5-bf58-602e6e19bd5b"
+F4_GUID = "35d6ef37-09f8-457a-bb6c-b467a5ddab68"
+F5_GUID = "9aeb1ae9-ce09-4d3b-a3ae-c11ef6b81b81"
+F6_GUID = "f9b9c190-bab3-4b8a-9217-4d196e5b0058"
+F7_GUID = "7e4f279c-ec7c-4036-8166-478b600ace35"
 GUID_ARR = [F0_GUID, F1_GUID, F2_GUID, F3_GUID, 
             F4_GUID, F5_GUID, F6_GUID, F7_GUID]
 
@@ -25,11 +25,13 @@ GST_STATIC = 0
 GST_COVER = 1
 GST_SPIN_RIGHT = 2
 GST_SPIN_LEFT = 3
+GST_COVER_OFF = 4
 
 sensor_number = 8
 filename = "C:\\HCI-final-project-master\\python\\arduino_buffer.txt"
 angle_diff = math.pi / 20
 current_angle = 0
+is_covered = False
 
 
 def isclose(a, b, tol=1e-5):
@@ -47,11 +49,12 @@ var_speed = 0.05
 while True:
     
     # changing random sin function
-    gh.SetSliderValue(VARIANCE_GUID, var_count)
-    if var_down:
-        var_count -= var_speed
-    else:
-        var_count += var_speed
+    if not is_covered:
+        gh.SetSliderValue(VARIANCE_GUID, var_count)
+        if var_down:
+            var_count -= var_speed
+        else:
+            var_count += var_speed
     
     if isclose(var_count, 2) or isclose(var_count, 3):
         var_down = not var_down
@@ -82,17 +85,22 @@ while True:
 #            if count[i] > 10:
 #                count[i] = 0
             
-    elif (gesture == GST_COVER):
-        pass
+    if gesture == GST_COVER:
+        is_covered = True
     
-    elif (gesture == GST_SPIN_RIGHT):
+    if gesture == GST_COVER_OFF:
+        is_covered = False
+    
+    if (gesture == GST_SPIN_RIGHT):
         current_angle += angle_diff
         if (current_angle > 2 * math.pi):
             current_angle = 0
         gh.SetSliderValue(ROTATE_GUID, current_angle)
     
-    elif (gesture == GST_SPIN_LEFT):
+    if (gesture == GST_SPIN_LEFT):
         current_angle -= angle_diff
+        if (current_angle <= 0 ):
+            current_angle = 2 * math.pi
         gh.SetSliderValue(ROTATE_GUID, current_angle)
     
     # update view
